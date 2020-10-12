7ac1881cefb10e6a9f88e95303998545afa86dec
unknown <shreejanayonzon@gmail.com>
2020-10-12 12:26:16 +0545

    added on model

--- EmployeeManagement/Models/Employee.cs
+++ EmployeeManagement/Models/Employee.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+
+
 public class Employee
 {
     public string FirstName { get; set; }
@@ -6,16 +8,20 @@ public class Employee
     public string Address { get; set; }
     public char Gender { get; set; } = 'M';
     public double Salary { get; set; }
-}
-public static List<Employee> GetEmployees()
-{
-    // Object initializer
-    var employee1 = new Employee { FirstName = "Shreejana", Surname = "Yonzon", Address = "Ktm", Salary = 100.0 };
-    var employee2 = new Employee { FirstName = "Ram", Surname = "Shrestha", Address = "Bhaktapur", Salary = 10000.0 };
-    var employee3 = new Employee { FirstName = "Sushmita", Surname = "Acharya", Address = "Ktm", Salary = 100.0 };
-    var employee4 = new Employee { FirstName = "Dinesh", Surname = "Ghimire", Address = "Ktm", Salary = 100.0 };
-    var employee5 = new Employee { FirstName = "Soham", Surname = "Neupane", Address = "Ktm", Salary = 100.0 };
-    var employee6 = new Employee { FirstName = "Bishal", Surname = "Rawal", Address = "Ktm", Salary = 100.0 };
+    public static List<Employee> GetEmployees()
+    {
+        // Object initializer
+        var employee1 = new Employee { FirstName = "Shreejana", Surname = "Yonzon", Address = "Ktm", Salary = 100.0 };
+        var employee2 = new Employee { FirstName = "Ram", Surname = "Shrestha", Address = "Bhaktapur", Salary = 10000.0 };
+        var employee3 = new Employee { FirstName = "Sushmita", Surname = "Acharya", Address = "Ktm", Salary = 100.0 };
+        var employee4 = new Employee { FirstName = "Dinesh", Surname = "Ghimire", Address = "Ktm", Salary = 100.0 };
+        var employee5 = new Employee { FirstName = "Soham", Surname = "Neupane", Address = "Ktm", Salary = 100.0 };
+        var employee6 = new Employee { FirstName = "Bishal", Surname = "Rawal", Address = "Ktm", Salary = 100.0 };
 
-    var employees = new List<Employee> { employee1, employee2, employee3, employee4, employee5, employee6 };
+        var employees = new List<Employee> { employee1, employee2, employee3, employee4, employee5, employee6 };
+        return employees;
+    }
+
 }
+
+
